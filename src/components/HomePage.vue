68b86ca41241494ab3816b8e946650cea81ac9fb
Jenette Sellin <jenettesellin@Jenettes-MacBook-Pro.local>
2026-01-23 23:42:03 -0800

    Update performance list

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -39,19 +39,20 @@
         <h3 class="header-font-left"> Upcoming </h3>
         <p class="paragraph-font-left indented-list">
            <!-- Edit below this line to add a new entry for an Upcoming Show -->
-            <strong> ⟡ 1/17/2026 </strong> - Berkeley Balkan Bacchanal, Starry Plough Pub, Berkeley, CA 
+            <strong> ⟡ 5/30/2026 </strong> - Third Annual Occidental World Music and Dance Festival, Occidental Center for the Arts, Occidental, CA <br>
         </p>
         <h3 class="header-font-left"> Past </h3>
         <p class="paragraph-font-left indented-list">
           <!-- Edit below this line to add a new entry for a Past Show -->
+             <strong> ⟡ 1/17/2026 </strong> - Berkeley Balkan Bacchanal, Starry Plough Pub, Berkeley, CA <br>
              <strong> ⟡ 10/17/25 </strong> - Bakers and Commons, Berkeley, CA <br>
              <strong> ⟡ 9/5/25 </strong> - Selkie Fest, Fremont, CA <br>
              <strong> ⟡ 7/19/25 </strong> - Berkeley Balkan Bacchanal, Starry Plough Pub, Berkeley, CA <br>
-             <strong> ⟡ 7/2/25 </strong> - Balkan Camp, Mendocino, CA <br>
-             <strong> ⟡ 6/7/25 </strong> - Occidental World Music Fest, Occidental, CA <br>
+             <strong> ⟡ 7/2/25 </strong> - EEFC Balkan Camp, Mendocino, CA <br>
+             <strong> ⟡ 6/7/25 </strong> - Second Annual Occidental World Music and Dance Festival, Occidental Center for the Arts, Occidental, CA <br>
              <strong> ⟡ 1/18/25 </strong> - Berkeley Balkan Bacchanal, Starry Plough Pub, Berkeley, CA <br>
-             <strong> ⟡ 12/31/24 </strong> - New Year’s Eve Balkan Bash, Ashkenaz, Berkeley, CA <br>
-             <strong> ⟡ 11/22/24 </strong> - Lost Church, San Francisco, CA <br>
+             <strong> ⟡ 12/31/24 </strong> - Balkan New Year’s Eve, Ashkenaz, Berkeley, CA <br>
+             <strong> ⟡ 11/22/24 </strong> - The Lost Church, San Francisco, CA <br>
              <strong> ⟡ 7/20/24 </strong> - Berkeley Balkan Bacchanal, Ashkenaz, Berkeley, CA <br>
              <strong> ⟡ 5/19/24 </strong> - Balkan Sundays, Bissap Baobab, San Francisco, CA <br>
              <strong> ⟡ 3/16/24 </strong> - Berkeley Balkan Bacchanal, Starry Plough Pub, Berkeley, CA <br>
